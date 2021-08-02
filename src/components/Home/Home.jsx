f29aec526dac508d57d0d299cf9a1e26dd7225e0
NAREK KARAPETYAN <krptnrkrptnrk@gmail.com>
2021-08-02 18:06:45 +0400

    feat: Navbar unfinished

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "../Authentication/Login/Login";
+import Navbar from "../Navbar/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Login />
+    </div>
+  );
+}
