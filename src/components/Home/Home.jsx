83c22f319149e2cacb41289f19e413b44434fea4
NAREK KARAPETYAN <krptnrkrptnrk@gmail.com>
2021-08-03 15:20:00 +0400

    refactor: some more reafctoring

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Login from "../Authentication/Login/Login";
 import Navbar from "../Navbar/Navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <Login />
     </div>
   );
 }
