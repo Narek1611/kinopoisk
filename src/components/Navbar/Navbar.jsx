c0a2edd23b4fafba9026be2c599777f761c38071
NAREK KARAPETYAN <krptnrkrptnrk@gmail.com>
2021-08-03 15:00:39 +0400

    refactor: deleted unnecessary code parts

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,12 +9,10 @@ import InputBase from '@material-ui/core/InputBase';
 import Badge from '@material-ui/core/Badge';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MailIcon from '@material-ui/icons/Mail';
 import NotificationsIcon from '@material-ui/icons/Notifications';
-import MoreIcon from '@material-ui/icons/MoreVert';
 import Button from '@material-ui/core/Button';
 
 
@@ -103,9 +101,7 @@ export default function PrimarySearchAppBar() {
     handleMobileMenuClose();
   };
 
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
+ 
 
   const menuId = 'primary-search-account-menu';
   const renderMenu = (
@@ -166,18 +162,11 @@ export default function PrimarySearchAppBar() {
 
   return (
     <div className={classes.grow}>
-      <AppBar title={<img src="https://lurkmore.so/images/a/aa/Kinopoisk_logo.png"/>}
+      <AppBar title={<img  alt="logo" src="https://lurkmore.so/images/a/aa/Kinopoisk_logo.png"/>}
       position="static">
         <Toolbar>
         <img src="https://lurkmore.so/images/a/aa/Kinopoisk_logo.png" width="50px" alt="logo" className={classes.logo} />
-          {/* <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton> */}
+          
           <Typography className={classes.title} variant="h6" noWrap>
             KINOPOISK
           </Typography>
@@ -196,43 +185,16 @@ export default function PrimarySearchAppBar() {
           </div>
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
-            {/* <IconButton aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton aria-label="show 17 new notifications" color="inherit">
-              <Badge badgeContent={17} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-              
-            </IconButton> */}
+            
             <Button variant="contained" color="primary" disableElevation>
-Favourite</Button>
+Favorite</Button>
             <Button variant="contained" color="primary" disableElevation>
 Log In</Button>
 
-            {/* <IconButton
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton> */}
+            
           </div>
           <div className={classes.sectionMobile}>
-            {/* <IconButton
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton> */}
+            
           </div>
         </Toolbar>
       </AppBar>
