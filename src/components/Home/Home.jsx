8875d35a90deb03fb3c2ba92d0e50118fdc43fff
NAREK KARAPETYAN <krptnrkrptnrk@gmail.com>
2021-08-03 19:26:39 +0400

    feat: got data from api and split into cards

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../Navbar/Navbar";
+import MovieCard from "../MovieCard/MovieCard"
+import { getMoviesByPage } from "../../service";
+import { useEffect } from "react";
 
 export default function Home() {
+  const [movies, setMovies] = useState([])
+useEffect(()=>{
+  getMoviesByPage(1).then(({results})=>{
+    setMovies(results)
+  })
+},[])
+console.log(movies);
   return (
     <div>
       <Navbar />
+      {
+        movies.map(movie=>{
+         return <MovieCard
+         key={movie.id}
+           title={movie.title}
+           path={movie.backdrop_path}
+           />
+
+        })
+      }
     </div>
   );
 }
