1b5494c47a4d57903dc586fcd6339eb05ef72091
NAREK KARAPETYAN <krptnrkrptnrk@gmail.com>
2021-08-04 20:25:24 +0400

    feat(MovieDetails): added MovieDetails design

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { getImgUrl, getMovieById } from "../../service";
-import Loading from '../Loading/Loading';
-import Box from '@material-ui/core/Box';
-import "./MovieDetails.css"
-
+import Loading from "../Loading/Loading";
+import Box from "@material-ui/core/Box";
+import "./MovieDetails.css";
 
 export default function MovieDetail() {
   let { id } = useParams();
@@ -17,11 +16,37 @@ export default function MovieDetail() {
   }, [id]);
 
   console.log(movieDetail);
-  return(
-    movieDetail.length === 0 ? (<Box className="backgroundLoading"><Loading /></Box>) :
-     <Box className="backgroundDetails">
+  return movieDetail.length === 0 ? (
+    <Box className="backgroundLoading">
+      <Loading />
+    </Box>
+  ) : (
+    <div>
       {/* <p>{movieDetail.title}</p> */}
-      <img src={getImgUrl(movieDetail.backdrop_path)} alt="gyago" width="100%"/>
-      </Box>
+      <img
+        className="backgroundDetails"
+        src={getImgUrl(movieDetail.backdrop_path)}
+        alt="gyago"
+        width="100%"
+      />
+      <div className="container">
+        <div className="poster">
+        <img
+        src={getImgUrl(movieDetail.poster_path)}
+        alt="gyago"
+      />
+        </div>
+        <div className="title">
+          <b>{movieDetail.title}</b>
+          </div>
+        <div className="info">
+          <b>About The Film</b>
+          <p>{movieDetail.vote_average}</p>
+          <i>
+          {movieDetail.overview}
+          </i>
+          </div>
+      </div>
+    </div>
   );
 }
